1d19877332a55ef11ddde309ecf536c0b9948b79
Piyush <piyushnavarkar@gmail.com>
2025-05-01 12:30:37 +0530

    MASTER COMMIT

---
+++ inventory-service/src/main/java/com/codingera/ecommerce/inventory_service/dto/OrderRequestDto.java
@@ -0,0 +1,11 @@
+package com.codingera.ecommerce.inventory_service.dto;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class OrderRequestDto {
+    private List<OrderRequestItemDto> items;
+}
+
---
+++ order-service/src/main/java/com/codingera/ecommerce/order_service/repoitory/OrdersRepository.java
@@ -0,0 +1,7 @@
+package com.codingera.ecommerce.order_service.repoitory;
+
+import com.codingera.ecommerce.order_service.entity.Orders;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrdersRepository extends JpaRepository<Orders, Long> {
+}
